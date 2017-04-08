24f1ae5e8c098e99e33ad25c818f4f0ea741df9c
Nick <nicklascook@hotmail.com>
2017-04-08 14:45:25 +1000

    .

--- style.js
+++ style.js
@@ -23,6 +23,7 @@
     });
 
     if(elem.value == undefined){
+      console.log(elem.value);
       elem.value = "Welcome to Notetab.\n\n Anything you type in here will be synced with Chrome.\n\n The toolbar on the right has a timer and notepads.\n Notepads function just like this main screen, but are like small independent versions.\n\n Check the Github for future features and updates:\n https://github.com/nicklascook/Notetab\n\n Tips & Features:\n Typing a backslash '/' on an empty line creates a separator or title line \n'esc' loses any currently open window."
     }
   }
